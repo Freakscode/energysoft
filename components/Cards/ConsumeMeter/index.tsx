0c89d2df72b3b842016adc1a4954893f8c0c56f8
Gabriel Cardona <gabcardona0782@gmail.com>
2024-08-06 17:09:53 -0500

    Implementación de obtención de promedio basado en los datos adquiridos de la API

--- components/Cards/ConsumeMeter/index.tsx
+++ components/Cards/ConsumeMeter/index.tsx
@@ -13,8 +13,7 @@ export async function ConsumeMeter() {
     }
     
     const {data: measures, error } = await supabase.from('medidas').select('id, Irms, potencia').gt('Irms', 0)
-    .order('id', {ascending: false})
-    .limit(10);
+    .order('id', {ascending: false});
 
     if (error){
         console.log('Error fetching measures: ', error.message);
@@ -27,14 +26,29 @@ export async function ConsumeMeter() {
     const irmsArray = measures?.map(measures => measures.Irms);
     const potenciaArray = measures?.map(measures => (measures.potencia*10).toFixed(2));
 
+    
+    function consumoPromedio(potenciaArray: string[]){
+        let sum = 0;
+        try {
+            potenciaArray.forEach((element) => {
+                sum += parseFloat(element);
+            });
+            return sum/potenciaArray.length;
+        }
+        catch (error) {
+            console.log('Error calculating average: ', error);
+            return undefined;
+        }
+    }
 
     return (
-        <div>
+        <main>
+            <div>
             <h1>Últimas 10 mediciones</h1>
             <div>
                 <h2>Irms</h2>
                 <ul>
-                    {irmsArray?.map((value, index) => (
+                    {irmsArray?.slice(-10).map((value, index) => (
                         <li key={index}>{value}</li>
                     ))}
                 </ul>
@@ -42,11 +56,16 @@ export async function ConsumeMeter() {
             <div>
                 <h2>Potencia</h2>
                 <ul>
-                    {potenciaArray?.map((value, index) => (
+                    {potenciaArray?.slice(-10).map((value, index) => (
                         <li key={index}>{value}</li>
                     ))}
                 </ul>
             </div>
-        </div>
+            <div>
+                <h2>Promedio de consumo en la última semana</h2>
+                <p>{consumoPromedio(potenciaArray ?? [])?.toFixed(2)}</p>
+            </div>
+            </div>
+        </main>
     );
 }
