4387753976576b9f6a249d3225497e89369a8ca2
Gabriel J. C. Osorio <gabcardona0782@gmail.com>
2024-08-24 11:29:48 -0500

    Auth runtime edge

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
